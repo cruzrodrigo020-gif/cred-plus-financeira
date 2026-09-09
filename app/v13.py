e20e9f46b0d447c4b10355275744efb83c4bf900
cruzrodrigo020-gif <cruzrodrigo020@gmail.com>
2026-09-08 22:58:50 -0300

    Conecta lembretes WhatsApp à aplicação

--- app/v13.py
+++ app/v13.py
@@ -1,6 +1,8 @@
 from app.v12 import app
 from app.v12_growth import router as growth_router
+from app.v13_whatsapp import router as whatsapp_router
 
 app.title = 'CRED+ Financeira Premium'
-app.version = '12.3.0'
+app.version = '12.4.0'
 app.include_router(growth_router)
+app.include_router(whatsapp_router)
