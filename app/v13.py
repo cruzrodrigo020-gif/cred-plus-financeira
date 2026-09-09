5fe0fd72a1b7554c1ec0c2f02d36b55fd9ffa83f
cruzrodrigo020-gif <cruzrodrigo020@gmail.com>
2026-09-08 23:06:10 -0300

    Conecta exclusão segura à API

--- app/v13.py
+++ app/v13.py
@@ -1,8 +1,10 @@
 from app.v12 import app
 from app.v12_growth import router as growth_router
 from app.v13_whatsapp import router as whatsapp_router
+from app.v13_delete import router as delete_router
 
 app.title = 'CRED+ Financeira Premium'
-app.version = '12.4.0'
+app.version = '12.5.0'
 app.include_router(growth_router)
 app.include_router(whatsapp_router)
+app.include_router(delete_router)
