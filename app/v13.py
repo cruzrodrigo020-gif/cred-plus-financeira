f93ac9920dfadd3572ab596a3907b5251f02820c
cruzrodrigo020-gif <cruzrodrigo020@gmail.com>
2026-09-09 15:17:58 -0300

    Publica link público de cadastro de clientes

--- app/v13.py
+++ app/v13.py
@@ -11,12 +11,14 @@ from app.v12 import app
 from app.v12_growth import router as growth_router
 from app.v13_whatsapp import router as whatsapp_router
 from app.v13_delete import router as delete_router
+from app.v14_public import router as public_router
 
 app.title = 'CRED+ Financeira Premium'
-app.version = '12.6.1'
+app.version = '12.7.0'
 app.include_router(growth_router)
 app.include_router(whatsapp_router)
 app.include_router(delete_router)
+app.include_router(public_router)
 
 
 @app.get('/cobrador')
@@ -27,3 +29,13 @@ def collector_app():
 @app.get('/cobrador/')
 def collector_app_slash():
     return FileResponse('app/static/collector.html')
+
+
+@app.get('/cadastro')
+def public_register():
+    return FileResponse('app/static/public-register.html')
+
+
+@app.get('/cadastro/')
+def public_register_slash():
+    return FileResponse('app/static/public-register.html')
