9c319ca665fdd0097e6f88584a20a647383ba2bc
cruzrodrigo020-gif <cruzrodrigo020@gmail.com>
2026-09-08 23:39:32 -0300

    Cria link público dedicado ao app do cobrador

--- app/v13.py
+++ app/v13.py
@@ -1,10 +1,22 @@
+from fastapi.responses import FileResponse
+
 from app.v12 import app
 from app.v12_growth import router as growth_router
 from app.v13_whatsapp import router as whatsapp_router
 from app.v13_delete import router as delete_router
 
 app.title = 'CRED+ Financeira Premium'
-app.version = '12.5.0'
+app.version = '12.6.0'
 app.include_router(growth_router)
 app.include_router(whatsapp_router)
 app.include_router(delete_router)
+
+
+@app.get('/cobrador')
+def collector_app():
+    return FileResponse('app/static/collector.html')
+
+
+@app.get('/cobrador/')
+def collector_app_slash():
+    return FileResponse('app/static/collector.html')
